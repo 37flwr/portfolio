3f54ed42e4ce8eea1c05bd0c4a7ef8958e093e24
37flwr <nesterishinyarik@gmail.com>
2024-03-05 01:18:53 +0200

    add double tap for phone usage

--- src/features/open-window/ui/OpenWindow.tsx
+++ src/features/open-window/ui/OpenWindow.tsx
@@ -1,7 +1,7 @@
 import { type ReactNode } from 'react'
+import { useDoubleTap } from 'use-double-tap'
 import { openWindow } from '../model/openWindow'
 import { NotoSansTextElement } from '@shared/ui/text-elements'
-
 import { ShortcutIcons, Shortcuts } from '@shared/types/Shortcuts.interface'
 import './styles.scss'
 
@@ -13,17 +13,15 @@ interface IOpenWindow {
 }
 
 function OpenWindow(windowInfo: IOpenWindow): ReactNode {
+    const bindDoubleClick = useDoubleTap(() =>
+        openWindow({
+            icon: windowInfo.iconTitle,
+            title: windowInfo.title,
+            application: windowInfo.application,
+        })
+    )
     return (
-        <button
-            className="desktop-icon"
-            onDoubleClick={() =>
-                openWindow({
-                    icon: windowInfo.iconTitle,
-                    title: windowInfo.title,
-                    application: windowInfo.application,
-                })
-            }
-        >
+        <button className="desktop-icon" {...bindDoubleClick}>
             <div className="desktop-icon__img">{windowInfo.Icon}</div>
             <NotoSansTextElement className="desktop-icon__title">
                 {windowInfo.title}
--- src/features/open-window/ui/OpenWindowFromFolder.tsx
+++ src/features/open-window/ui/OpenWindowFromFolder.tsx
@@ -1,15 +1,14 @@
 import { type ReactNode } from 'react'
+import { useDoubleTap } from 'use-double-tap'
 import { openWindow } from '../model/openWindow'
 import { NotoSansTextElement } from '@shared/ui/text-elements'
 import { RenderOpenWindow } from '@shared/types/Window.interface'
 import './styles.scss'
 
 function OpenWindowFromFolder(windowInfo: RenderOpenWindow): ReactNode {
+    const bindDoubleClick = useDoubleTap(() => openWindow({ ...windowInfo }))
     return (
-        <button
-            className="folder-icon"
-            onDoubleClick={() => openWindow({ ...windowInfo })}
-        >
+        <button className="folder-icon" {...bindDoubleClick}>
             <div className="folder-icon__img">{windowInfo.Icon}</div>
             <NotoSansTextElement className="folder-icon__title">
                 {windowInfo.title}
